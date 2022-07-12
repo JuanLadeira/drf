ee24b99eacad196f8b24af69c66bf152b86d53c1
juan <juan.fll@hotmail.com>
2022-07-12 12:57:33 -0300

    inclusão do basic.py e do urls.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,13 @@
 from django.http import JsonResponse
 import json
+from products.models import Product
 # Create your views here.
 
 def api_home(request, *args, **kwargs):
-    body = request.body
+    model_data = Product.objects.all().order.by('?').first()
     data = {}
-    try:
-        data= json.loads(body)
-    except:
-        pass
-    print(data)
+    if model_data:
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse({"message": "Hi there, this is your django API response"})
