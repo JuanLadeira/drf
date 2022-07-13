eaed6056c78cb074a6eca3ee2445fc167bdb152e
juan <juan.fll@hotmail.com>
2022-07-13 12:57:58 -0300

    Inclusão do metodo perform_create da view ProductListCreateAPIView.

--- backend/products/views.py
+++ backend/products/views.py
@@ -10,13 +10,20 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content')
+        if content is None:
+            content = title
+        serializer.save(content=content)
 
-class ProductDetailAPIView(generics.RetrieveAPIView): #MOSTRA OS DETALHES DOS PRODUTOS
+
+class ProductDetailAPIView(generics.RetrieveAPIView):  # MOSTRA OS DETALHES DOS PRODUTOS
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class ProductListAPIView(generics.ListAPIView):
-    #NÃO SERÁ USADO.
+    # NÃO SERÁ USADO.
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
